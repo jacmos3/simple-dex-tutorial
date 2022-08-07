191a1edf1be26fc0e7dcf47c119c82b988457c5d
Jacopo <jacopogithub.pbcc5@aleeas.com>
2022-08-07 11:06:20 +0200

    added MyTransactions module and decorations

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,6 +4,7 @@ import {exchangeSelector} from '../store/selectors';
 import {loadAllOrders} from '../store/interactions';
 import Trades from './Trades.js';
 import OrderBook from './OrderBook.js';
+import MyTransactions from './MyTransactions.js'
 
 class Content extends Component{
   componentDidMount() {
@@ -37,7 +38,9 @@ class Content extends Component{
             </div>
           </div>
         </div>
+
         <OrderBook />
+
         <div className="vertical-split">
           <div className="card bg-dark text-white">
             <div className="card-header">
@@ -48,15 +51,7 @@ class Content extends Component{
               <a href="/#" className="card-link">Card link</a>
             </div>
           </div>
-          <div className="card bg-dark text-white">
-            <div className="card-header">
-              Card Title
-            </div>
-            <div className="card-body">
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card content.</p>
-              <a href="/#" className="card-link">Card link</a>
-            </div>
-          </div>
+          <MyTransactions />
         </div>
          <Trades />
       </div>
